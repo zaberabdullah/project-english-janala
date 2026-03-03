e099e1b61f9dba8880e43dcd0b8ac2e903040146
zaberabdullah <zaberabdullah120@gmail.com>
2026-03-03 15:36:29 +0600

    dynamically added the cards...

--- script/index.js
+++ script/index.js
@@ -18,7 +18,15 @@ const displayLevelWord = (words) => {
       console.log(word);
       const card = document.createElement("div");
       card.innerHTML = `
-      <p>cat</p>
+       <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+        <h2 class="font-bold text-2xl">Eager</h2>
+        <p class="font-semibold">Meaning /Pronounciation</p>
+        <div class="font-bangla text-2xl font-medium">"আগ্রহী / ইগার"</div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>
       `
       wordContainer.append(card);
     });
