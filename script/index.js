9f192f53981d49cc003d78d6be24f770c468ef57
zaberabdullah <zaberabdullah120@gmail.com>
2026-03-03 09:08:16 +0600

    create div to show card dynamically

--- script/index.js
+++ script/index.js
@@ -4,6 +4,26 @@ const loadLessons = () => {
     .then((json) => displayLessons(json.data));
 };
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+    words.forEach((word) => {
+      console.log(word);
+      const card = document.createElement("div");
+      card.innerHTML = `
+      <p>cat</p>
+      `
+      wordContainer.append(card);
+    });
+};
+
 const displayLessons = (lessons) => {
   // 1. get the container & empty it
   const levelContainer = document.getElementById("level-container");
@@ -13,10 +33,12 @@ const displayLessons = (lessons) => {
     // 3. create Element
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-          <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`;
+          <button onClick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`;
         // 4. append into the container
         levelContainer.appendChild(btnDiv);
   }
 };
+
+
 
 loadLessons();
