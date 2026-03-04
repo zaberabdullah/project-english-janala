ae3b3f4fa1cd9d898b9a454aeeb053c9163a3c43
zaberabdullah <zaberabdullah120@gmail.com>
2026-03-04 11:14:39 +0600

    handle some errors/empty lessons for better UX

--- script/index.js
+++ script/index.js
@@ -15,6 +15,17 @@ const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container");
     wordContainer.innerHTML = "";
 
+    if(words.length === 0){
+      wordContainer.innerHTML = `
+      <div class="text-center col-span-full rounded-xl space-y-6 py-10 ">
+      <img class="mx-auto" src="./assets/alert-error.png" />
+        <p class="text-lg font-medium text-gray-500 font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="text-4xl font-medium font-bangla">নেক্সট Lesson এ যান</h2>
+      </div>
+      `
+      return;
+    }
+
 // {
 //     "id": 19,
 //     "level": 1,
@@ -28,9 +39,9 @@ const displayLevelWord = (words) => {
       const card = document.createElement("div");
       card.innerHTML = `
        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
         <p class="font-semibold">Meaning /Pronounciation</p>
-        <div class="font-bangla text-2xl font-medium">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="font-bangla text-2xl font-medium">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"}"</div>
         <div class="flex justify-between items-center">
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
